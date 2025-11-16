bf38adce0f55cd3b41b618308555b0ea72ded531
Shouq-Alsulami <sulami.shouq@gmail.com>
2025-11-16 06:34:29 +0300

    updated

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/wajeeh/', // Use your repo name here
+  plugins: [react()]
 });
